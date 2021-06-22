d5eead5c2c15715a264b09cbb5063915b7339d5e
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-06-22 17:53:15 +0200

    Update challenge-25.js

--- challenge-25/challenge-25.js
+++ challenge-25/challenge-25.js
@@ -1,16 +1,16 @@
 /*
-Essa semana você terá dois desafios:
-1) Revisar todo o contéudo passado até aqui, e ver se você realmente entendeu
-tudo o que foi passado! Se tiver dúvidas, anote, e então abra issues,
-ou comente no seu pull request mesmo, que eu irei ajudá-lo a entender
-o que não ficou tão claro das aulas anteriores.
-É essencial que você entenda todo o conteúdo que foi passado até aqui,
-para que possamos prosseguir para a parte mais avançada do curso :D
+Esta semana tendrás dos retos:
+1) Repasa todo el contenido que has cubierto hasta ahora, y comprueba si realmente entiendes
+¡todo lo que has pasado! Si tienes preguntas, escríbelas y luego abre los temas,
+o comentar tu pull request, y te ayudaré a entender
+lo que no estaba tan claro en las lecciones anteriores.
+Es esencial que entiendas todo el contenido que se ha tratado hasta ahora,
+para poder pasar a la parte más avanzada del curso :D
 
-2) Estudar eventos!
-Acesse a página do MDN:
+2) ¡Estudie los eventos!
+Acceda a la página de MDN:
 https://developer.mozilla.org/en-US/docs/Web/Events#Categories
 
-Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
-desafio os experimentos legais que você conseguir desenvolver :D
+Intenta aplicar en la práctica algunos de los eventos que hay y postea en este
+desafiar los experimentos geniales que puedes desarrollar :D
 */
