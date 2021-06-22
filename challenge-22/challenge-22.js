e72ea4567688e9cd5858d9653fc7e2c763ec5214
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-06-22 17:54:43 +0200

    Update challenge-22.js

--- challenge-22/challenge-22.js
+++ challenge-22/challenge-22.js
@@ -1,70 +1,70 @@
   /*
-  Crie dois objetos, que serão duas pessoas. Cada um deve ter as propriedades
-  `name` e `lastName`, preenchidos com o nome e sobrenome da pessoa.
+  Crea dos objetos, que serán dos personas. Cada uno debe tener las propiedades
+  Las propiedades `name` y `lastName`, rellenadas con el nombre y los apellidos de la persona.
   */
   // ?
 
   /*
-  Agora crie uma função chamada `getFullName` que retorne as propriedades
-  `name` e `lastName` dos objetos acima, formando um nome completo.
-  A função não deve receber nenhum parâmetro, mas as propriedades `name` e
-  `lastName` devem ser dinâmicas.
-  A mesma função deve servir para as duas pessoas (ou qualquer outra que for
-  criada).
-  Depois disso, invoque essa função, mostrando no console o nome completo das
-  pessoas que foram criadas anteriormente, passando as pessoas acima como
-  contexto da função. Use um console.log por pessoa.
+  Ahora cree una función llamada `getFullName` que devuelva el
+  `nombre` y `apellido` de los objetos anteriores, haciendo un nombre completo.
+  La función no debe tomar ningún parámetro, pero el `nombre` y
+  `apellido` debe ser dinámico.
+  La misma función debería funcionar para ambas personas (o cualquier otra cosa que
+  creado).
+  Después de esto, invoca esta función, mostrando a la consola los nombres completos de los
+  personas que creó anteriormente, pasando las personas anteriores como el
+  contexto de la función. Utilice un console.log por persona.
   */
-  console.log( 'O nome das pessoas é:' );
+  console.log( 'El nombre de la persona es:' );
   // ?
 
   /*
-  Crie uma função chamada `sum`. Essa função pode receber uma lista de
-  parâmetros variável, e deverá retornar a soma de todos eles.
-  Não use estruturas de repetição para somar os argumentos.
-  Na primeira linha, dentro da função, deixe um console.log para mostrar todos
-  os parâmetros passados para essa função.
-  */
-  // ?
-
-  /*
-  Mostre no console que a função acima funciona, invocando-a em 3 console.log
-  diferentes, com quantidades variáveis de parâmetros passados.
-  */
-  console.log( '\nSomar alguns números:' );
-  // ?
-
-  /*
-  Declare uma variável chamada `userEntry`, que irá receber alguns valores
-  entrados pelo usuário. Mostre para o usuário a seguinte frase:
-  "Entre com alguns números que serão somados:"
+  Crea una función llamada `suma`. Esta función puede tomar una lista variable de
+  parámetros de la variable, y debe devolver la suma de todos ellos.
+  No utilices estructuras de bucle para sumar los argumentos.
+  En la primera línea, dentro de la función, deja un console.log para mostrar todo
+  los parámetros pasados a esta función.
   */
   // ?
 
   /*
-  Mostre no console o valor entrado pelo usuário:
+  Muestra en la consola que la función anterior funciona, invocándola en 3 console.log diferentes
+  con una cantidad variable de parámetros que se le pasan.
   */
-  console.log( '\nEntrada do usuário:' );
+  console.log( '\N- Guardar algunos números:' );
   // ?
 
   /*
-  Crie uma função chamada `justNumbers`, que recebe por parâmetro uma string
-  e remove tudo o que não for número, retornando um array somente com os números
-  da string. Mostre a representação em string dessa função no console.
+  Declarar una variable llamada `userEntry` que recibirá algunos valores
+  introducido por el usuario. Muestre al usuario la siguiente frase:
+  "Introduzca algunos números que se sumarán:"
   */
-  console.log( '\nFunção que limpa entrada do usuário (somente números):' );
   // ?
 
   /*
-  Usando a função acima, faça a limpeza dos valores entrados pelo usuário,
-  atribuindo o resultado à uma variável `numbers`.
+  Muestra el valor introducido por el usuario en la consola:
   */
-  console.log( '\nEntrada do usuário limpa. Somente números:' );
+  console.log( '\NIntroducción del usuario:' );
   // ?
 
   /*
-  Agora com o array de números, utilize a função `sum` para somar todos os
-  números desse array e mostre o resultado no console.
+  Cree una función llamada `justNumbers` que tome una cadena como parámetro
+  y elimina todo lo que no es un número, devolviendo una matriz con sólo los números
+  de la cadena. Muestra la representación de la cadena de esta función en la consola.
   */
-  console.log( '\nSomar números entrados pelo usuário:' );
+  console.log( '\NFunción que borra la entrada del usuario (sólo números):' );
+  // ?
+
+  /*
+  Utilizando la función anterior, limpie los valores introducidos por el usuario,
+  asignando el resultado a una variable "números".
+  */
+  console.log( '\NLa entrada del usuario se ha borrado. Sólo números:' );
+  // ?
+
+  /*
+  Ahora con el array de números, utiliza la función `sum` para sumar todos los
+  suma todos los números de esa matriz y muestra el resultado en la consola.
+  */
+  console.log( '\NSuma de números introducidos por el usuario:' );
   // ?
