a5d69879e7996ef420dabcf3bfd732c31ed533fd
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-06-22 03:52:01 +0200

    Update challenge-31.js

--- challenge-31/challenge-31.js
+++ challenge-31/challenge-31.js
@@ -1,14 +1,11 @@
 /*
-Agora vamos criar a funcionalidade de "remover" um carro. Adicione uma nova
-coluna na tabela, com um botão de remover.
-
-Ao clicar nesse botão, a linha da tabela deve ser removida.
-
-Faça um pull request no seu repositório, na branch `challenge-31`, e cole
-o link do pull request no `console.log` abaixo.
-
-Faça um pull request, também com a branch `challenge-31`, mas no repositório
-do curso, para colar o link do pull request do seu repo.
+Ahora vamos a crear la funcionalidad para "eliminar" un coche. Añadir un nuevo
+en la tabla, con un botón de eliminación.
+Al hacer clic en ese botón se eliminará la fila de la tabla.
+Haz un pull request a tu repositorio, rama `desafío-31`, y pega
+el enlace del pull request al `console.log` de abajo.
+Haz un pull request, también con la rama `reto-31`, pero en el repositorio del curso
+para pegar el enlace del pull request de su repo.
 */
 
-console.log('Link do pull request do seu projeto');
+console.log('Enlace desde el pull request de tu proyecto');
