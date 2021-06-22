5faaa7630fd53c9761256517200d43401d95ecb7
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-06-22 03:56:52 +0200

    Update challenge-08.js

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -1,75 +1,75 @@
-/*
-Declare uma variável chamada `sum` e atribua a ela uma função chamada
-`calculateSum`. A função deve receber dois parâmetros e retornar a soma
-desses parâmetros.
+ /*
+Declarar una variable llamada `suma` y asignarle una función llamada
+`calcularSuma`. La función debe tomar dos parámetros y devolver la suma
+de estos parámetros.
 */
 // ?
 
 /*
-Invoque a função criada acima, passando dois números que serão somados, e mostre
-o resultado no console, com a frase:
-"A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
+Invoca la función creada anteriormente, pasando dos números que serán sumados, y muestra
+el resultado en la consola, con la frase
+"La suma de [VALOR1] y [VALOR2] es igual a [RESULTADO]".
 */
 // ?
 
 /*
-Mostre no console o nome da função criada acima, com a frase:
-"O nome da função que faz a soma é [NOME DA FUNÇÃO]."
+Mostrar el nombre de la función creada anteriormente en la consola, con la sentencia:
+"El nombre de la función que hace la suma es [NOMBRE DE LA FUNCIÓN]".
 */
 // ?
 
 /*
-Crie uma função literal chamada `showName`. Essa função deve retornar o
-seu nome.
+Crea una función literal llamada `showName`. Esta función debe devolver
+tu nombre.
 */
 // ?
 
 /*
-Declare uma variável chamada `varShowName` que recebe a função criada acima.
+Declara una variable llamada `varShowName` que recibe la función creada anteriormente.
 */
 // ?
 
 /*
-Usando a variável criada acima, mostre no console o nome e o retorno da função
-atribuída a ela, com a seguinte frase:
-"A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
+Utilizando la variable creada anteriormente, muestra el nombre y el retorno de la función
+asignado, con la siguiente declaración:
+"La función [NOMBRE DE LA FUNCIÓN] devuelve [RETORNO DE LA FUNCIÓN]".
 */
 // ?
 
 /*
-Crie uma função literal chamada `calculator`, que funcione assim:
-- A função deve receber um parâmetro que dirá qual operação matemática ela
-vai efetuar. Será uma string com os valores `+`, `-`, `*`, `/` ou `%`;
-- Essa função deve retornar uma segunda função que fará o seguinte:
-  - Essa segunda função deve receber dois parâmetros;
-  - Esses dois parâmetros serão os operandos usados na operação matemática;
-  - O retorno dessa segunda função é a operação matemática completa, com a frase:
-  "Resultado da operação: [NUMERO1] [OPERADOR] [NUMERO2] = [RESULTADO]."
-  - Se o operador não for válido, retornar a frase:
-  "Operação inválida."
+Crea una función literal llamada `calculadora` que funcione así:
+- La función debe recibir un parámetro que le indique qué operación matemática debe realizar
+que va a realizar. Será una cadena con los valores `+`, `-`, `*`, `/` o `%`;
+- Esta función debe devolver una segunda función que hará lo siguiente:
+  - Esta segunda función debe recibir dos parámetros;
+  - Estos dos parámetros serán los operandos utilizados en la operación matemática;
+  - El retorno de esta segunda función es la operación matemática completa, con el enunciado
+  "Resultado de la operación: [NÚMERO1] [OPERADOR] [NÚMERO2] = [RESULTADO]".
+  - Si el operador no es válido, devuelve la sentencia:
+  "Operación inválida".
 */
 // ?
 
 /*
-Declare uma variável chamada `sum`, que receberá a função acima, passando como
-parâmetro o operador de soma.
+Declarar una variable llamada `suma`, que recibirá la función anterior, pasando como
+operador de suma como parámetro.
 */
 // ?
 
 /*
-Agora `sum` é uma função. Mostre no console a soma de dois números, usando ela.
+Ahora `suma` es una función. Muestra la suma de dos números en la consola, utilizándola.
 */
 // ?
 
 /*
-Agora, declare algumas variáveis com os nomes `subtraction`, `multiplication`,
-`division` e `mod`, e atribua a elas a função `calculator`, passando o operador
-correto por parâmetro para cada uma delas.
+Ahora, declara algunas variables llamadas `substracción`, `multiplicación`,
+subdivisión` y `mod`, y asignarles la función `calculadora`, pasándoles la
+como parámetro de cada uno de ellos.
 */
 // ?
 
 /*
-Faça uma operação com cada uma das funções criadas acima, mostrando o resultado
-no console.
+Realiza una operación en cada una de las funciones creadas anteriormente, mostrando el resultado
+en la consola.
 */
 // ?
