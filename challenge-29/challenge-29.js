ab0e41316288819d69da7944e5c86538e505be12
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-06-22 17:51:19 +0200

    Update challenge-29.js

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -1,39 +1,39 @@
 (function() {
-  'use strict';
+  'uso estricto
 
   /*
-  Vamos estruturar um pequeno app utilizando módulos.
-  Nosso APP vai ser um cadastro de carros. Vamos fazê-lo por partes.
-  A primeira etapa vai ser o cadastro de veículos, de deverá funcionar da
-  seguinte forma:
-  - No início do arquivo, deverá ter as informações da sua empresa - nome e
-  telefone (já vamos ver como isso vai ser feito)
-  - Ao abrir a tela, ainda não teremos carros cadastrados. Então deverá ter
-  um formulário para cadastro do carro, com os seguintes campos:
-    - Imagem do carro (deverá aceitar uma URL)
+  Estructuremos una pequeña aplicación utilizando módulos.
+  Nuestra aplicación será un registro de coches. Lo haremos por partes.
+  El primer paso será la matriculación del vehículo, que debería funcionar de la siguiente manera
+  de la siguiente manera:
+  - Al principio del archivo debe tener la información de su empresa - nombre y
+  teléfono (ya veremos cómo se hace)
+  - Al abrir la pantalla, todavía no tenemos ningún coche registrado. Entonces debes tener
+  un formulario para la matriculación del coche, con los siguientes campos:
+    - Imagen del coche (debe aceptar una URL)
     - Marca / Modelo
-    - Ano
+    - Año / Año
     - Placa
-    - Cor
-    - e um botão "Cadastrar"
+    - Color
+    - y un botón de "Cadastrar".
 
-  Logo abaixo do formulário, deverá ter uma tabela que irá mostrar todos os
-  carros cadastrados. Ao clicar no botão de cadastrar, o novo carro deverá
-  aparecer no final da tabela.
+  Justo debajo del formulario, debe haber una tabla que muestre todos los
+  coches matriculados. Al pulsar el botón de registro, el nuevo coche debería
+  aparecen al final de la tabla.
 
-  Agora você precisa dar um nome para o seu app. Imagine que ele seja uma
-  empresa que vende carros. Esse nosso app será só um catálogo, por enquanto.
-  Dê um nome para a empresa e um telefone fictício, preechendo essas informações
-  no arquivo company.json que já está criado.
+  Ahora tienes que dar un nombre a tu aplicación. Imagina que es un
+  empresa que vende coches. Esta aplicación nuestra será sólo un catálogo por ahora.
+  Dale un nombre y un número de teléfono ficticio, rellenando esta información
+  en el archivo company.json ya creado.
 
-  Essas informações devem ser adicionadas no HTML via Ajax.
+  Esta información debe ser añadida en el HTML vía Ajax.
 
   Parte técnica:
-  Separe o nosso módulo de DOM criado nas últimas aulas em
-  um arquivo DOM.js.
+  Separar nuestro módulo DOM creado en las últimas clases en
+  un archivo DOM.js.
 
-  E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
-  que será nomeado de "app".
+  Y aquí en este archivo, hacer la lógica para registrar los coches, en un módulo
+  que se llamará "app".
   */
 
 })();
