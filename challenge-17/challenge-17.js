c4af204658f73e407df7b7aa64096d4197cb3b22
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-06-22 17:48:27 +0200

    Update challenge-17.js

--- challenge-17/challenge-17.js
+++ challenge-17/challenge-17.js
@@ -1,93 +1,93 @@
 /*
-1. Envolva todo o conteúdo desse desafio em uma IIFE.
-2. Adicione a diretiva 'use strict';
-3. Crie um arquivo index.html e adicione esse script à ele.
+Envuelve todo el contenido de este reto en un IIFE.
+2. Añade la directiva 'use strict';
+3. Cree un archivo index.html y añada este script a él.
 */
 
 /*
-Em todos os exercícios desse desafio, nós vamos utilizar expressões
-regulares! Para isso, iremos usar o texto abaixo. Coloque-o em uma
-variável chamada `text`:
-"Manuel Marques de Sousa, Conde de Porto Alegre (Rio Grande, 13 de junho de 1804 – Rio de Janeiro, 18 de julho de 1875), apelidado de "O Centauro de Luvas", foi um militar, político, abolicionista e monarquista brasileiro."
+En todos los ejercicios de este reto, vamos a utilizar
+¡expresiones! Para ello, utilizaremos el texto que aparece a continuación. Póngalo en un
+variable llamada `texto`:
+"Manuel Marques de Sousa, Conde de Porto Alegre (Río Grande, 13 de junio de 1804 - Río de Janeiro, 18 de julio de 1875), apodado "El Centauro de Guante", fue un militar, político, abolicionista y monárquico brasileño".
 */
 // ?
 
 /*
-Vamos começar com umas brincadeiras fáceis :D
-Troque o nome "Manuel Marques de Sousa" pelo seu nome, e mostre o resultado
-no console:
+Empecemos con algunos chistes fáciles :D
+Sustituye el nombre "Manuel Marques de Sousa" por tu nombre, y muestra el resultado
+en la consola:
 */
-console.log( 'Adicionando seu nome no texto:' );
+console.log( 'Añadiendo su nombre en el texto:' );
 // ?
 
 /*
-Agora, substitua a palavra "brasileiro" por sua cidade natal e mostre no
-console.
-Ex: Se você for da São Paulo, substitua por "paulista".
+Ahora, sustituye la palabra "brasileño" por tu ciudad natal y muestra en el
+consola.
+Ej: Si eres de São Paulo, sustituye por "paulista".
 */
-console.log( '\nTrocando naturalidade:' );
+console.log( '\N-volviendo a la naturalidad:' );
 // ?
 
 /*
-Substitua todos os números por um traço `-`. Cada caractere de número deve
-ser um traço. Mostre o resultado no console:
+Sustituye todos los números por un guión `-`. Cada carácter numérico debe
+ser una raya. Muestra el resultado en la consola:
 */
-console.log( '\nTrocando números por -:' );
+console.log( '\NIntercambio de números por -:' );
 // ?
 
 /*
-Substitua todas as letras (somente letras) de "D" maiúsculo até "h"
-minúsculo por "0" (número zero). Mostre o resultado no console:
+Reemplazar todas las letras (sólo las letras) desde la "D" mayúscula hasta la "h" minúscula
+con "0" (número cero). Muestra el resultado en la consola:
 */
-console.log( '\nTrocando de "D" a "h" por "0":' );
+console.log( '\NSustituir "D" por "h" minúscula por "0":' );
 // ?
 
 /*
-Substitua todos os "A" (maiúsculos ou minúsculos) por "4".
-Mostre o resultado no console:
+Sustituya todas las "A" (mayúsculas o minúsculas) por "4".
+Muestra el resultado en la consola:
 */
-console.log( '\nTrocando "A" e "a" por "4":' );
+console.log( '\NCambiando "A" y "a" por "4":' );
 // ?
 
 /*
-Substitua a frase "O Centauro de Luvas", deixando-a em caixa alta, usando
-o método `toUpperCase()`. Mostre o resultado no console:
+Sustituye la frase "El centauro de los guantes" por mayúsculas utilizando
+el método `toUpperCase()`. Muestra el resultado en la consola:
 */
-console.log( '\n"O Centauro de Luvas" em caixa alta:' );
+console.log( '\N-"El centauro de guante" en mayúsculas:' );
 // ?
 
 /*
-Agora iremos substituir as datas no formato "13 de junho de 1804" para
-"13/06/1804". A primeira coisa a fazer é criar uma função chamada
-`getMonthNumber`, que irá receber um parâmetro (será o nome do mês) e deverá
-retornar o número correspondente a esse mês.
-Ex: Se o usuário entrar com "março", deve retornar "03" (em string mesmo).
-Números com menos de dois dígitos devem ter um zero na frente.
-Crie então a função e mostre no console os retornos para os meses de março,
-setembro e dezembro.
-Use um console.log para cada mês, usando a frase:
-"O mês de [NOME DO MÊS] é representado pelo número [NÚMERO DO MÊS]."
+Ahora sustituiremos las fechas con el formato "13 de junio de 1804" por
+"13/06/1804". Lo primero que hay que hacer es crear una función llamada
+Tomará un parámetro (el nombre del mes) y deberá
+devuelve el número correspondiente a ese mes.
+Ej: Si el usuario introduce "marzo", debería devolver "03" (en cadena de todos modos).
+Los números con menos de dos dígitos deben llevar un cero delante.
+Entonces, crea la función y muestra en la consola los rendimientos de los meses de marzo,
+Septiembre y diciembre.
+Utilice un console.log para cada mes, utilizando la declaración
+"El mes de [NOMBRE DEL MES] está representado por el número [NÚMERO DEL MES]".
 */
-console.log( '\nMeses representados por números:' );
+console.log( '\NMeses representados por números:' );
 // ?
 
 /*
-Agora, declare uma variável chamada `regexDate` que irá receber a expressão
-regular que irá fazer o match com as datas. Crie grupos de captura para o
-dia, o mês e o ano. Para os meses, você pode fazer o match somente com os
-meses que estão no texto, não precisa adicionar todos.
-Com o que vimos até agora, você consegue fazer :D
-Mostre a regex no console.
+Ahora, declare una variable llamada `regexDate` que recibirá la expresión regular
+que coincida con las fechas. Crear grupos de captura para el
+día, mes y año. En el caso de los meses, sólo puede coincidir con los meses que aparecen en el texto.
+meses que están en el texto, no es necesario añadirlos todos.
+Con lo que hemos visto hasta ahora, puedes hacerlo :D
+Mostrar la regex en la consola.
 */
-console.log( '\nRegex que vai fazer o match com as datas do texto:' );
+console.log( '\NRegex que coincidirá con las fechas del texto:' )
 // ?
 
 /*
-Agora crie a função que irá fazer o replace dos dados. A função será chamada
-de `replaceDate`. Ela deve retornar a data no formato:
-"[DIA]/[MÊS]/[ANO]"
-Após criar a função, faça o replace das datas no texto, mostrando no
-console o resultado.
+Ahora crea la función que hará el reemplazo de datos. La función se llamará
+`replaceDate`. Debería devolver la fecha en el formato:
+"[DAY]/[MONTH]/[YEAR]"
+Después de crear la función, sustituye las fechas en el texto, mostrando el resultado en el
+consolar el resultado.
 */
-console.log( '\nReplace de datas:' );
+console.log( '\N-Sustituir fechas:' );
 // ?
